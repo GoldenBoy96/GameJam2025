bf0d4eb2e4e896e8d81722c05c3af1812f417bb4
Đỗ Long Ánh <98139573+Jangkuz@users.noreply.github.com>
2025-01-14 17:41:00 +0700

    add: Interactable cube

---
+++ GameJam2025/Assets/_Scripts/Controllers/Logics/DemoInteractableObject/AnhInteract.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AnhInteract : MonoBehaviour, IInterable
+{
+    [SerializeField]private float CooldownSpaghetti = 5f;
+    private float _currentCooldown;
+
+    public float Cooldown => throw new System.NotImplementedException();
+
+    private void FixedUpdate()
+    {
+        _currentCooldown -= Time.deltaTime;
+    }
+    public void Interact()
+    {
+        if(_currentCooldown > 0)
+        {
+            return;
+        }
+
+        Debug.Log($"Anh Interact {gameObject.name}");
+        _currentCooldown = CooldownSpaghetti;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Interact();
+    }
+}
