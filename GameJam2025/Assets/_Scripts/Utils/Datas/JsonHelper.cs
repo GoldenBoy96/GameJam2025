864a3dce4cb3b86951e73eefe90984f44dc0f180
Cậu vàng <huuminhgm@gmail.com>
2025-01-16 23:09:00 +0700

    update json util, add nuget for unity package

--- GameJam2025/Assets/_Scripts/Utils/Datas/JsonHelper.cs
+++ GameJam2025/Assets/_Scripts/Utils/Datas/JsonHelper.cs
@@ -1,18 +1,34 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
 using UnityEngine;
+using Formatting = Newtonsoft.Json.Formatting;
 
-public class JsonHelper : MonoBehaviour
+
+public class JsonHelper
 {
-    // Start is called before the first frame update
-    void Start()
+    protected JsonHelper() { }
+
+
+    public static T ReadData<T>(string filePath)
     {
-        
+        string jsonRead;
+        try
+        {
+            jsonRead = System.IO.File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<T>(jsonRead);
+        }
+        catch (FileNotFoundException)
+        {
+            return default;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void SaveData(Object saveObject, string filePath)
     {
-        
+        string json = JsonConvert.SerializeObject(saveObject, Formatting.Indented);
+        System.IO.File.WriteAllText(filePath, json);
     }
 }
