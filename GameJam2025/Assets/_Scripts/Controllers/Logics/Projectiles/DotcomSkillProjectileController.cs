3252532a1a1b4b90ddfe4698b2a916b3c516afb5
Vo Do Quang Duong <quangduongaptx4869@gmail.com>
2025-01-19 05:23:25 +0700

    add explpsion sound

---
+++ GameJam2025/Assets/_Scripts/Controllers/Logics/Projectiles/DotcomSkillProjectileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+public class DotcomSkillProjectileController : BaseProjectileController
+{
+    public void PlayAttackEffect()
+    {
+
+        Debug.Log("Quả cầu phát nổ!");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            PlayAttackEffect();
+            Destroy(gameObject); // Huỷ quả cầu sau khi va chạm
+        }
+    }
+}
--- GameJam2025/Assets/_Scripts/Utils/Constants/AudioConstants.cs
+++ GameJam2025/Assets/_Scripts/Utils/Constants/AudioConstants.cs
@@ -1,4 +1,15 @@
 public static class AudioConstants
 {
     public static readonly string DEMO_SOUND = "DEMO_SOUND";
+    public static readonly string BUBBLE_BULLET = "BUBBLE_BULLET";
+    public static readonly string BUBBLE_SHORTGUN = "BUBBLE_SHORTGUN";
+    public static readonly string DOULINGO_COMPLETE = "DOULINGO_COMPLETE";
+    public static readonly string DOULINGO_RIGHT = "DOULINGO_RIGHT";
+    public static readonly string DOULINGO_WRONG = "DOULINGO_WRONG";
+    public static readonly string DOULINGO_SUPER_WRONG = "DOULINGO_SUPER_WRONG";
+    public static readonly string SHIELD_BROKEN = "SHIELD_BROKEN";
+    public static readonly string CITY_OF_TEAR = "CITY_OF_TEAR";
+    public static readonly string DECISIVE_BATTLE = "DECISIVE_BATTLE";
+    public static readonly string BUMP = "BUMP";
+    public static readonly string EXPLOSION_SOUND = "EXPLOSION_SOUND";
 }
