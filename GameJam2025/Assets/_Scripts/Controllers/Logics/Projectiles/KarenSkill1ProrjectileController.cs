ba2b9336713f902a4124cfdc208429545d75e4ab
Đỗ Long Ánh <98139573+Jangkuz@users.noreply.github.com>
2025-01-18 07:28:53 +0700

    add: init Karen code

---
+++ GameJam2025/Assets/_Scripts/Controllers/Logics/Projectiles/KarenSkill1ProrjectileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class KarenSkill1ProrjectileController : BaseCharacterController
+    {
+    public Vector3 dir = Vector3.left;
+    [SerializeField] string content = string.Empty;
+
+
+    private TextMeshPro textMest;
+    private void Start()
+    {
+        textMest = GetComponentInChildren<TextMeshPro>();
+        textMest.text = content;    
+        StartCoroutine(WaitToDestroy());
+    }
+    void Update()
+    {
+        transform.Translate(dir * 10 * Time.deltaTime);
+    }
+    IEnumerator WaitToDestroy()
+    {
+        //TODO: Chinh thoi gian mem trong model
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+    }
+    }
